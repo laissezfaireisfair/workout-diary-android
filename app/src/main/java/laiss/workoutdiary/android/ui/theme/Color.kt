107268ef71791c79b8eb0f7a74451fc0059e6dce
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-12-07 13:04:31 +0700

    WIP on color scheme

--- app/src/main/java/laiss/workoutdiary/android/ui/theme/Color.kt
+++ app/src/main/java/laiss/workoutdiary/android/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package laiss.workoutdiary.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Base = Color(0xff24273a)
+val Crust = Color(0xff181926)
+val Mantle = Color(0xff1e2030)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Surface0 = Color(0xff363a4f)
+val Surface1 = Color(0xff494d64)
+val Surface2 = Color(0xff5b6078)
+
+val Overlay0 = Color(0xff6e738d)
+val Overlay1 = Color(0xff8087a2)
+val Overlay2 = Color(0xff939ab7)
+
+val Text = Color(0xffcad3f5)
+val Subtext0 = Color(0xffa5adcb)
+val Subtext1 = Color(0xffb8c0e0)
+
+val Red = Color(0xffed8796)
+val Green = Color(0xffa6da95)
+val Yellow = Color(0xffeed49f)
+val Blue = Color(0xff8aadf4)
+val Lavender = Color(0xffb7bdf8)
+val Rosewater = Color(0xfff4dbd6)
+val Peach = Color(0xfff5a97f)
+val Flamingo = Color(0xfff0c6c6)
+val Pink = Color(0xfff5bde6)
--- app/src/main/java/laiss/workoutdiary/android/ui/theme/Theme.kt
+++ app/src/main/java/laiss/workoutdiary/android/ui/theme/Theme.kt
@@ -12,15 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Crust,
+    secondary = Mantle,
+    tertiary = Surface0,
+    background = Base,
+    error = Red,
+    surface = Surface1
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Base,
+    secondary = Crust,
+    tertiary = Mantle
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
